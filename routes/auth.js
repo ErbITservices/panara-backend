29c2f308e11b3bdf4e0efdb52643b6dde0cb7bfd
MIHIRPATEL2715 <mihirpqtel@gmail.com>
2024-12-09 14:06:04 +0530

    mihir

--- routes/auth.js
+++ routes/auth.js
@@ -121,7 +121,7 @@ router.post("/forgotpass", async (req, res)=> {
     if(!user) return res.status(401).json({sucess: false,message: "user with this email not exist"});
   
     //sending email thing
-    const resetURl = `https://panarait.in/api/auth/resetpassword/${resetToken}`;
+    const resetURl = `https://panarait.in/resetpassword/${resetToken}`;
     
     const emailtext = `
       you have requested a password reset
